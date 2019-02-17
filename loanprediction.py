abba8426e9bdac5d6ac225f0211188c3ea37ae1b
Mansimran Anand <mansimransinghanand@gmail.com>
2019-02-17 15:18:49 +0530

    started work on missing data

--- loanprediction.py
+++ loanprediction.py
@@ -11,3 +11,24 @@ import matplotlib.pyplot as plt
 
 
 dataset=pd.read_csv('Train.csv')
+
+
+#taking are of missing data 
+
+from sklearn.preprocessing import Imputer 
+imputer = Imputer(missing_values='NaN', strategy = 'mean', axis=0)
+
+x_train = dataset.iloc[:,7:9]
+y_train = dataset.iloc[:,12:13]
+
+imputer = imputer.fit(x_train)
+x_train= imputer.transform(x_train)
+
+dataset.describe()
+c=0
+for i in len(x_train):
+    if x_train[8][i]=='nan':
+        c+=1
+x_train
+
+
